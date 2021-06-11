f7534072f6b1f470c3182be2bee8a4ee418c3703
Vicki Crosson <vicki.crosson@gmail.com>
2021-06-11 11:13:42 -0400

    paths

---
+++ src/paths.py
@@ -0,0 +1,7 @@
+#local_directory = "/Users/206790/Dropbox/Apps/QuidditchTracker/S18/"
+local_directory = "/Users/viccro/Documents/OtherStuff/racery-scrape/quidditch-tracker/data/S21-botf-finals/"
+dropbox_directory = "/racery-tracker/S21-botf-finals/"
+
+teamsFile = "/Users/viccro/Documents/OtherStuff/racery-scrape/quidditch-tracker/webpages/S21-botf-finals-teams.html"
+baseUrl = "https://rti.racery.com/r/Battle-Fandoms-Season-2-Finals/"
+
